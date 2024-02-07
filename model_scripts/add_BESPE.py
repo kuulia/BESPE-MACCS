643b2462c7a937d44278e686754562503bbbfebd
kuulia <linus.lind00@gmail.com>
2024-02-07 10:38:47 +0200

    improve readibility

--- model_scripts/add_BESPE.py
+++ model_scripts/add_BESPE.py
@@ -10,6 +10,9 @@ from os import path
 # groups to exclude from 1, 2-4, >4 features
 EXCLUDE = ['carbon number', 'oxygen count']
 
+def remove_zero_cols(df: pd.DataFrame) -> pd.DataFrame:
+    return df.loc[:, (df != 0).any(axis=0)]
+
 # Does the molecule contain exactly one count of this SMARTS pattern?
 def one(df: pd.DataFrame) -> pd.DataFrame:
     fp = pd.DataFrame()
@@ -17,6 +20,7 @@ def one(df: pd.DataFrame) -> pd.DataFrame:
     for group in smarts_patterns:
         if (group not in EXCLUDE):
             fp[group] = np.where(df[group] == 1, 1, 0)
+    fp = remove_zero_cols(fp)
     return fp
 
 # Does the molecule contain 2, 3 or 4 counts of this SMARTS pattern?
@@ -26,8 +30,8 @@ def two_to_four(df: pd.DataFrame) -> pd.DataFrame:
     for group in smarts_patterns:
         if (group not in EXCLUDE):
             fp[f'({group})_2-4'] = np.where((df[group] >= 2)\
-                                                  & (df[group] <= 4), 1, 0)
-    fp = fp.loc[:, (fp != 0).any(axis=0)] #remove zero-columns
+                                          & (df[group] <= 4), 1, 0)
+    fp = remove_zero_cols(fp)
     return fp
 
 # Does the molecule contain more than four counts of this SMARTS pattern?
@@ -37,7 +41,7 @@ def more_than_four(df: pd.DataFrame) -> pd.DataFrame:
     for group in smarts_patterns:
         if (group not in EXCLUDE):
             fp[f'({group})_4plus'] = np.where((df[group] > 4), 1, 0)
-    fp = fp.loc[:, (fp != 0).any(axis=0)]
+    fp = remove_zero_cols(fp)
     return fp
 
 # Takes enumeration and converts to bits-bit representation 
@@ -49,8 +53,8 @@ def binary_encoded(df: pd.DataFrame, \
     bin_enc = pd.DataFrame()
     for i in range(0,bits+1):
         bin_enc[f'{name}_bit{bits+1-i}'] = df[element]\
-            .apply(np.binary_repr, width = bits+1)\
-            .map(lambda v: v[i])
+                .apply(np.binary_repr, width = bits+1)\
+                .map(lambda v: v[i])
     return bin_enc
 
 def main():
@@ -67,16 +71,17 @@ def main():
     bespe_two_to_four = two_to_four(smarts)
     bespe_four_plus = more_than_four(smarts)
     carbons = binary_encoded(smarts, 'carbon number', 'C', 5) # 5-bit encoding 
-    oxygens = binary_encoded(smarts, 'oxygen count', 'O', 5) # 5-bit encoding 
+    oxygens = binary_encoded(smarts, 'oxygen count', 'O', 5) 
 
     # combine features
-    bespe = bespe_one_plus.join([bespe_two_to_four, carbons, oxygens, bespe_four_plus])
+    bespe = bespe_one_plus.join([bespe_two_to_four, carbons, 
+                                 oxygens, bespe_four_plus])
 
     # load MACCS fingerprint file
     maccs_fp = pd.read_csv(path.join(filepath, 'MACCS.txt'), 
                            header=None, sep=' ')
 
-    # append new features to MACCS
+    # append new features to MACCS and save output
     maccs_and_bespe = maccs_fp.join(bespe)
     fileoutname =  f'data/BESPE-MACCS.txt'
     np.savetxt(fileoutname, maccs_and_bespe, fmt = "%s")
